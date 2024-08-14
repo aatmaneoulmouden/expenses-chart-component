78e74fa48cdeb1230725b0a8f1bfdef5adb9463b
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-14 19:37:43 +0100

    Build balance component

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,5 +1,15 @@
+import logo from "../assets/logo.svg";
+
 const Balance = () => {
-  return <div>Balance</div>;
+  return (
+    <div className="flex justify-between items-center bg-primary-softRed p-5 rounded-2xl">
+      <div className="text-neutral-veryPaleOrange">
+        <p>My balance</p>
+        <h1 className="text-3xl font-semibold tracking-[1px] mt-1">$921.48</h1>
+      </div>
+      <img src={logo} alt="Expenses Chart Component" />
+    </div>
+  );
 };
 
 export default Balance;
