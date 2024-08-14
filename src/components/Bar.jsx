24303e28b3d4e36c4ae4c462ca72b8f25db125b0
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-14 20:35:49 +0100

    Add dynamic data

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,5 +1,20 @@
-const Bar = () => {
-  return <div>Bar</div>;
+const Bar = ({ day, amount }) => {
+  const barStyles = {
+    height: `${amount * 2}px`,
+  };
+  return (
+    <article className="flex-1 flex flex-col items-center gap-1 group cursor-pointer">
+      <div
+        className="bar w-full bg-primary-softRed rounded-md relative"
+        style={barStyles}
+      >
+        <div className="text-xs text-neutral-veryPaleOrange font-medium p-2 bg-neutral-darkBrown rounded-md absolute -top-10 left-1/2 -translate-x-1/2 hidden group-hover:block">
+          ${amount}
+        </div>
+      </div>
+      <span className="label text-sm text-neutral-mediumBrown">{day}</span>
+    </article>
+  );
 };
 
 export default Bar;
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,36 +1,17 @@
+import chartData from "../data/data.json";
+import Bar from "./Bar";
+
 const Chart = () => {
+  const displayBars = chartData.map((bar, index) => (
+    <Bar key={index} {...bar} />
+  ));
   return (
     <div className="pb-5 border-b border-neutral-cream mb-5">
-      <h2 className="text-2xl font-bold text-neutral-darkBrown mb-10">Spending - Last 7 days</h2>
-      <div className="chart flex gap-3 items-end">
-        <article>
-          <div className="bar h-11"></div>
-          <span className="label">mon</span>
-        </article>
-        <article>
-          <div className="bar h-14"></div>
-          <span className="label">tue</span>
-        </article>
-        <article>
-          <div className="bar h-20"></div>
-          <span className="label">wed</span>
-        </article>
-        <article>
-          <div className="bar h-24"></div>
-          <span className="label">thu</span>
-        </article>
-        <article>
-          <div className="bar h-16"></div>
-          <span className="label">fri</span>
-        </article>
-        <article>
-          <div className="bar h-28"></div>
-          <span className="label">sat</span>
-        </article>
-        <article>
-          <div className="bar h-5"></div>
-          <span className="label">sun</span>
-        </article>
+      <h2 className="text-2xl font-bold text-neutral-darkBrown mb-10">
+        Spending - Last 7 days
+      </h2>
+      <div className="chart flex gap-3 items-end h-40">
+        {displayBars}
       </div>
     </div>
   );
